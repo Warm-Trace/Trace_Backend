ef93e1cfd31515dc5adb34afa6c6625a67aeea01
seobeeeee1001 <wjsbdcindsu@pukyong.ac.kr>
2025-07-10 15:54:48 +0900

    #1 [CHORE] 푸시 테스트

---
+++ src/main/java/com/example/trace/report/controller/ReportController.java
@@ -0,0 +1,12 @@
+package com.example.trace.report.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/reports")
+@RequiredArgsConstructor
+public class ReportController {
+
+}
