5459b9fda886afdc99da48f244510394b65bea2f
seobeeeee1001 <wjsbdcindsu@pukyong.ac.kr>
2025-07-11 16:42:01 +0900

    #1 [CHORE] 신고 API에 SWAGGER 적용

--- src/main/java/com/example/trace/report/controller/ReportController.java
+++ src/main/java/com/example/trace/report/controller/ReportController.java
@@ -4,6 +4,8 @@ import com.example.trace.auth.dto.PrincipalDetails;
 import com.example.trace.global.response.ApiResponse;
 import com.example.trace.report.dto.ReportRequest;
 import com.example.trace.report.service.ReportService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -15,10 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/reports")
 @RequiredArgsConstructor
+@Tag(name = "신고 API", description = "신고 관련 API")
 public class ReportController {
     private final ReportService reportService;
 
-    @PostMapping
+    @PostMapping(consumes = "application/json")
+    @Operation(summary = "신고 생성", description = "사용자가 신고를 생성합니다.")
     public ResponseEntity<ApiResponse> createReport(
             @AuthenticationPrincipal PrincipalDetails principalDetails,
             @RequestBody ReportRequest request) {
