ccd4df704564a31cca1c724a1ba2382c607f833a
gyunho <gyunh01@pukyong.ac.kr>
2025-07-17 01:24:48 +0900

    #10 [REFACTOR] 게시글 수정 시 이미지 삭제&추가가 가능하도록 변경
    - 삭제할 이미지는 s3와 db에서 제거
    - 추가할 이미지는 s3에 업로드 후 post와 연관관계 매핑
    - 요청 dto 수정

--- src/main/java/com/example/trace/post/service/PostService.java
+++ src/main/java/com/example/trace/post/service/PostService.java
@@ -3,22 +3,26 @@ package com.example.trace.post.service;
 import com.example.trace.gpt.dto.VerificationDto;
 import com.example.trace.post.dto.cursor.CursorResponse;
 import com.example.trace.post.dto.cursor.PostCursorRequest;
-import com.example.trace.post.dto.post.PostFeedDto;
-import com.example.trace.post.dto.post.PostUpdateDto;
 import com.example.trace.post.dto.post.PostCreateDto;
 import com.example.trace.post.dto.post.PostDto;
+import com.example.trace.post.dto.post.PostFeedDto;
+import com.example.trace.post.dto.post.PostUpdateDto;
 import com.example.trace.user.User;
+import java.util.List;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface PostService {
 
-    PostDto createPost(PostCreateDto postCreateDto,Long userId);
+    PostDto createPost(PostCreateDto postCreateDto, Long userId);
 
     PostDto createPost(PostCreateDto postCreateDto, String ProviderId, VerificationDto verificationDto);
 
     PostDto getPostById(Long id, User user);
 
-    PostDto updatePost(Long id, PostUpdateDto postUpdateDto,String providerId);
+    PostDto updatePost(Long id, PostUpdateDto postUpdateDto, List<MultipartFile> imageFiles, String providerId);
+
     CursorResponse<PostFeedDto> getAllPostsWithCursor(PostCursorRequest request, String requesterId);
+
     void deletePost(Long id, String providerId);
 
     CursorResponse<PostFeedDto> searchPostsWithCursor(PostCursorRequest request, String providerId);
--- src/main/java/com/example/trace/post/service/PostServiceImpl.java
+++ src/main/java/com/example/trace/post/service/PostServiceImpl.java
@@ -1,36 +1,38 @@
 package com.example.trace.post.service;
 
+import com.example.trace.auth.repository.UserRepository;
 import com.example.trace.emotion.EmotionService;
 import com.example.trace.emotion.EmotionType;
 import com.example.trace.emotion.dto.EmotionCountDto;
+import com.example.trace.file.FileType;
+import com.example.trace.file.S3UploadService;
 import com.example.trace.global.errorcode.PostErrorCode;
 import com.example.trace.global.exception.PostException;
 import com.example.trace.gpt.domain.Verification;
 import com.example.trace.gpt.dto.VerificationDto;
 import com.example.trace.gpt.service.PostVerificationService;
 import com.example.trace.mission.repository.DailyMissionRepository;
+import com.example.trace.post.domain.Post;
+import com.example.trace.post.domain.PostImage;
 import com.example.trace.post.domain.PostType;
 import com.example.trace.post.domain.cursor.SearchType;
 import com.example.trace.post.dto.cursor.CursorResponse;
 import com.example.trace.post.dto.cursor.PostCursorRequest;
-import com.example.trace.post.dto.post.PostFeedDto;
-import com.example.trace.user.User;
-import com.example.trace.file.FileType;
-import com.example.trace.file.S3UploadService;
-import com.example.trace.post.dto.post.PostUpdateDto;
-import com.example.trace.post.domain.Post;
-import com.example.trace.post.domain.PostImage;
 import com.example.trace.post.dto.post.PostCreateDto;
 import com.example.trace.post.dto.post.PostDto;
-import com.example.trace.auth.repository.UserRepository;
+import com.example.trace.post.dto.post.PostFeedDto;
+import com.example.trace.post.dto.post.PostUpdateDto;
+import com.example.trace.post.repository.PostImageRepository;
 import com.example.trace.post.repository.PostRepository;
+import com.example.trace.user.User;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -38,18 +40,19 @@ import java.util.List;
 @Slf4j
 public class PostServiceImpl implements PostService {
 
+    private static final int MAX_IMAGES = 5;
+
     private final PostRepository postRepository;
     private final UserRepository userRepository;
     private final S3UploadService s3UploadService;
     private final PostVerificationService postVerificationService;
     private final EmotionService emotionService;
     private final DailyMissionRepository dailyMissionRepository;
-
-    private static final int MAX_IMAGES = 5;
+    private final PostImageRepository postImageRepository;
 
     @Override
     @Transactional
-    public PostDto createPost(PostCreateDto postCreateDto,Long userId) {
+    public PostDto createPost(PostCreateDto postCreateDto, Long userId) {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new PostException(PostErrorCode.USER_NOT_FOUND));
 
@@ -67,14 +70,13 @@ public class PostServiceImpl implements PostService {
 
     @Override
     @Transactional
-    public PostDto createPost(PostCreateDto postCreateDto, String ProviderId,VerificationDto verificationDto) {
+    public PostDto createPost(PostCreateDto postCreateDto, String ProviderId, VerificationDto verificationDto) {
         User user = userRepository.findByProviderId(ProviderId)
                 .orElseThrow(() -> new PostException(PostErrorCode.USER_NOT_FOUND));
 
-        if(verificationDto != null){
+        if (verificationDto != null) {
             user.updateVerification(verificationDto);
         }
-
 
         if (postCreateDto.getContent() == null || postCreateDto.getContent().isEmpty()) {
             throw new PostException(PostErrorCode.CONTENT_EMPTY);
@@ -85,10 +87,9 @@ public class PostServiceImpl implements PostService {
         }
 
         Verification verification = null;
-        if(verificationDto != null){
+        if (verificationDto != null) {
             verification = postVerificationService.makeVerification(verificationDto);
         }
-
 
         Post post = Post.builder()
                 .postType(postCreateDto.getPostType())
@@ -97,13 +98,13 @@ public class PostServiceImpl implements PostService {
                 .content(postCreateDto.getContent())
                 .user(user)
                 .verification(verification)
-                .missionContent(postCreateDto.getPostType() == PostType.MISSION ? postCreateDto.getMissionContent() : null)
+                .missionContent(
+                        postCreateDto.getPostType() == PostType.MISSION ? postCreateDto.getMissionContent() : null)
                 .build();
 
-        if(verification !=null){
+        if (verification != null) {
             verification.connectToPost(post);
         }
-
 
         Post savedPost = postRepository.save(post);
 
@@ -133,7 +134,7 @@ public class PostServiceImpl implements PostService {
 
     @Override
     @Transactional
-    public PostDto getPostById (Long postId, User requestUser){
+    public PostDto getPostById(Long postId, User requestUser) {
         Post post = postRepository.findById(postId)
                 .orElseThrow(() -> new PostException(PostErrorCode.POST_NOT_FOUND));
 
@@ -141,7 +142,7 @@ public class PostServiceImpl implements PostService {
 
         EmotionCountDto emotionCountDto = emotionService.getEmotionCountsByType(postId);
 
-        EmotionType yourEmotionType = emotionService.getYourEmotion(postId,requestUser);
+        EmotionType yourEmotionType = emotionService.getYourEmotion(postId, requestUser);
 
         PostDto postDto = PostDto.fromEntity(post);
 
@@ -162,13 +163,12 @@ public class PostServiceImpl implements PostService {
         List<PostFeedDto> posts;
         if (request.getCursorDateTime() == null || request.getCursorId() == null) {
             // 첫 페이지 조회
-            posts = postRepository.findPostsWithCursor(null,null, size + 1, request.getPostType(),providerId);
+            posts = postRepository.findPostsWithCursor(null, null, size + 1, request.getPostType(), providerId);
         } else {
             // 다음 페이지 조회
             posts = postRepository.findPostsWithCursor(
-                    request.getCursorDateTime(), request.getCursorId(), size + 1, request.getPostType(),providerId);
+                    request.getCursorDateTime(), request.getCursorId(), size + 1, request.getPostType(), providerId);
         }
-
 
         // 다음 페이지 여부 확인
         boolean hasNext = false;
@@ -199,7 +199,6 @@ public class PostServiceImpl implements PostService {
     @Transactional(readOnly = true)
     public CursorResponse<PostFeedDto> searchPostsWithCursor(PostCursorRequest request, String providerId) {
         int size = request.getSize() != null ? request.getSize() : 10;
-
 
         // 검색어가 있는 경우 검색 메서드 사용, 없으면 기존 메서드 사용
         List<PostFeedDto> posts;
@@ -255,15 +254,31 @@ public class PostServiceImpl implements PostService {
 
     @Override
     @Transactional
-    public PostDto updatePost (Long id, PostUpdateDto postUpdateDto, String providerId){
+    public PostDto updatePost(Long id, PostUpdateDto postUpdateDto, List<MultipartFile> imageFiles, String providerId) {
+        List<String> removal = postUpdateDto.getRemoval();
+        List<String> images = new ArrayList<>();
         Post post = postRepository.findById(id)
                 .orElseThrow(() -> new PostException(PostErrorCode.POST_NOT_FOUND));
 
-        if (!post.getUser().getProviderId().equals(providerId)) {
+        if (!post.isOwner(providerId)) {
             throw new PostException(PostErrorCode.POST_UPDATE_FORBIDDEN);
         }
 
-        post.editPost(postUpdateDto.getTitle(), postUpdateDto.getContent());
+        if (removal != null && !removal.isEmpty()) {
+            s3UploadService.deleteFiles(removal); // S3에서 이미지 삭제
+            postImageRepository.deleteAllByImageUrlIn(removal); // DB에서 이미지 행 삭제
+        }
+
+        if (imageFiles != null && !imageFiles.isEmpty()) {
+            try {
+                // 추가할 이미지를 s3에 업로드 후 이미지 링크들을 리스트로 받아옴
+                images.addAll(s3UploadService.savePostFiles(imageFiles, FileType.POST, providerId));
+            } catch (IOException e) {
+                throw new PostException(PostErrorCode.POST_IMAGE_UPLOAD_FAILED);
+            }
+        }
+
+        post.editPost(postUpdateDto.getTitle(), postUpdateDto.getContent(), PostImage.listOf(images));
 
         Post updatedPost = postRepository.save(post);
         return PostDto.fromEntity(updatedPost);
