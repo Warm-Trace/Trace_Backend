b2669bef31c3bad36fbfe41bee71d134466f1b30
gyunho <gyunh01@pukyong.ac.kr>
2025-08-18 14:27:39 +0900

    [FIX] User를 user.domain에서 import하도록 수정

--- src/main/java/com/example/trace/notification/repository/NotificationSettingRepository.java
+++ src/main/java/com/example/trace/notification/repository/NotificationSettingRepository.java
@@ -1,7 +1,7 @@
 package com.example.trace.notification.repository;
 
 import com.example.trace.notification.domain.NotificationSetting;
-import com.example.trace.user.User;
+import com.example.trace.user.domain.User;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
